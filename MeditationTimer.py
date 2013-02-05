9daac1755111ada10c948f1a7c9b1b0bd8e4aa24
Adam Menges <adammenges@gmail.com>
2013-02-05 11:58:55 -0700

    Improved comments

--- MeditationTimer.py
+++ MeditationTimer.py
@@ -10,7 +10,7 @@ currentsystem = str(platform.node()).lower()
 # I wanted the time to be in the center of the screen, hacky as it is, I'm just using spaces. These refer to the machine names of my laptop and desktop (because they have different screen sizes), change these for your machine.
 spaceing = "                                                                                        "
 if('frankfurt' in currentsystem): spaceing = "                                                                                                                      "
-if('server' in currentsystem): spaceing = "                                                                                                                      "
+if('gutenberg' in currentsystem): spaceing = "                                                                                                                      "
 
 # Print once each second. 600 seconds is 10 minutes.
 for i in range(600):
