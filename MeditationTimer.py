8e072d4c1bebc9420ba537d2f696065ab5641add
Adam Menges <adammenges@gmail.com>
2013-02-16 07:51:42 -0700

    Added the ability to specify the time to run.

--- MeditationTimer.py
+++ MeditationTimer.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 import time
 import platform
+import sys
 
 starttime = int(time.time())
 currentsystem = str(platform.node()).lower()
+mins = 10
+try: mins = int(sys.argv[1])
+except: pass
 
 # I wanted the time to be in the center of the screen,
 # hacky as it is, I'm just using spaces. These refer to
@@ -12,15 +16,16 @@ currentsystem = str(platform.node()).lower()
 # your machine, of course.
 spaceing = "                                                                                        "
 if('frankfurt' in currentsystem): spaceing = "                                                                                                                      "
+if('server' in currentsystem): spaceing = "                                                                                                                      "
 if('gutenberg' in currentsystem): spaceing = "                                                                                        "
 
-# Print once each second. 600 seconds is 10 minutes.
-for i in range(600):
+# Print once each second. 10 minutes, unless otherwise specified.
+for i in range(60 * mins):
     currenttime = int(time.time())
     print spaceing + str(currenttime-starttime)
     time.sleep(1)
 
 # Print blank lines so the text above it slowly fades away.
-for i in range(100):
+for i in range(200):
     print ""
     time.sleep(1)
